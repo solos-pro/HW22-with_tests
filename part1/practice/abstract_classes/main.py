78aac27826291ac4416207e1c1f9b7bae0f1dbc5
solos-pro <sergey-solonichenko@rambler.ru>
2022-04-02 21:00:07 +0300

    abstract classes

--- part1/practice/abstract_classes/main.py
+++ part1/practice/abstract_classes/main.py
@@ -59,6 +59,56 @@ class Transport(ABC):
         pass
 
 
+class Boat(Transport):
+    def start_engine(self):
+        print('Катер громко затарахтел')
+
+    def stop_engine(self):
+        print('Двигатель катера чихнул напоследок и заглох')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+
+class Car(Transport):
+    def start_engine(self):
+        print('Машина заурчала двигателем')
+
+    def stop_engine(self):
+        print('Двигатель катера чихнул напоследок и заглох')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print('Катер громко затарахтел')
+
+    def stop_engine(self):
+        print('Двигатель катера чихнул напоследок и заглох')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+
+class Person:
+
+    def use_transport(self, transport: Transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
+
 
 
 # Отрезок кода для самопроверки.
