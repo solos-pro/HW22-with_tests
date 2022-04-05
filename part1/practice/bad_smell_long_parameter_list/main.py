d384b5c5fd52ff4dc68ea789513d41213102afbb
solos-pro <sergey-solonichenko@rambler.ru>
2022-04-05 18:54:23 +0300

    bad smell: long_parameter_list

--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -11,39 +11,37 @@
 
 
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
+    def __init__(self):
+        self._speed = 1
+        self.new_x = 0
+        self.new_y = 0
 
+    def get_speed(self, is_fly, crawl):
+        if is_fly:
+            self._speed = 1.2
+        elif crawl:
+            self._speed = 0.5
         if is_fly and crawl:
             raise ValueError('Рожденный ползать летать не должен!')
+        else:
+            self._speed = 1
+        return self._speed
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
+    def move(self, field, x_coord, y_coord, direction):
+
+        if direction == 'UP':
+            self.new_y = y_coord + self._speed
+            self.new_x = x_coord
+        elif direction == 'DOWN':
+            self.new_y = y_coord - self._speed
+            self.new_x = x_coord
+        elif direction == 'LEFT':
+            self.new_y = y_coord
+            self.new_x = x_coord - self._speed
+        elif direction == 'RIGTH':
+            self.new_y = y_coord
+            self.new_x = x_coord + self._speed
+
 
             field.set_unit(x=new_x, y=new_y, unit=self)
 
